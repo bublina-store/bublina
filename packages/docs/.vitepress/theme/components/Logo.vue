8db13e840f57eb2b7d45406e3bd0cff6cbacd674
TomasDurica <tomas.durica.sk@gmail.com>
2023-03-07 03:43:06 +0100

    Added docs logo

--- packages/docs/.vitepress/theme/components/Logo.vue
+++ packages/docs/.vitepress/theme/components/Logo.vue
@@ -1,9 +1,109 @@
 <script setup lang="ts">
-  import { ref } from 'vue'  
+  import { ref, computed, watchEffect } from 'vue'  
+  import { useMouseInElement } from '@vueuse/core'
+  
+  const body = ref<HTMLElement>()
+  const highlight = ref<HTMLElement>()
+  const leftEye = ref<HTMLElement>()
+  const rightEye = ref<HTMLElement>()
 
-  const canvas = ref<HTMLCanvasElement>()
+  const { x, y } = useMouseInElement()
+    
+  const offset = computed(() => {
+    const bbox = highlight.value?.getBoundingClientRect()
+    if (!bbox) {
+      return [0, 0]
+    }
+    
+    const centerX = bbox.left + bbox.width / 2
+    const centerY = bbox.top + bbox.height / 2
+    
+    const offsetX = x.value - centerX
+    const offsetY = y.value - centerY
+    
+    const distance = Math.sqrt(offsetX * offsetX + offsetY * offsetY)
+    
+    return [offsetX / distance, offsetY / distance, distance]
+  })
+  
+  watchEffect(() => {
+    if (!leftEye.value || !rightEye.value || !offset.value) {
+      return
+    }
+    
+    const [offsetX, offsetY, distance] = offset.value
+    
+    const relativeDistance = Math.min(distance, 1000) / 1000
+    
+    const bodyDistance = relativeDistance * 120    
+    body.value.style.left = `${offsetX * bodyDistance}px`
+    body.value.style.top = `${offsetY * bodyDistance}px`    
+    
+    const highlightDistance = relativeDistance * 10
+    highlight.value.style.left = `${offsetX * highlightDistance - 4}px` 
+    highlight.value.style.top = `${offsetY * highlightDistance - 4}px` 
+    
+    const leftEyeDistance = relativeDistance * 100    
+    leftEye.value.style.left = `${offsetX * leftEyeDistance}px`
+    leftEye.value.style.top = `${offsetY * leftEyeDistance + offsetX * relativeDistance * 16}px`
+
+    const rightEyeDistance = relativeDistance * 100
+    rightEye.value.style.left = `${offsetX * rightEyeDistance}px`
+    rightEye.value.style.top = `${offsetY * rightEyeDistance - offsetX * relativeDistance * 16}px`
+  })
+  
 </script>
 
 <template>
-  <canvas ref="canvas" width="320" height="320" />
+  <div class="logo">
+    <div ref="body" class="body">
+      <div ref="highlight" class="highlight">
+        <div ref="leftEye" class="left eye"></div>
+        <div ref="rightEye" class="right eye"></div>
+      </div>
+    </div>
+  </div>
 </template>
+
+<style scoped>
+.logo {
+  --ink-color: #001e2d;
+  --border-color: #007fb0;
+  --highlitght-color: #81cfff;
+  --body-size: 320px;
+  --ink-size: 16px;
+  --eye-size: 32px;
+}
+
+.body {
+  position: relative;
+  width: var(--body-size);
+  height: var(--body-size);
+  border: var(--ink-size) solid var(--ink-color);
+  border-radius: 50%;
+  box-shadow: #81cfff 0 0 64px 0;
+} 
+
+.highlight {
+  position: relative;
+  display: grid;
+  place-items: center;
+  grid-template-columns: 1fr 1fr;
+  padding: 25%;
+  z-index: -1;
+  width: calc(100% + var(--ink-size) / 2);
+  height: calc(100% + var(--ink-size) / 2);
+  border: var(--ink-size) solid var(--border-color);
+  background: var(--highlitght-color);
+  border-radius: 50%;
+}
+
+.eye {
+  position: relative;
+  width: var(--eye-size);
+  height: var(--eye-size);
+  border-radius: 50%;
+  background: var(--ink-color);
+  transition: top 0.05s ease, left 0.05s ease;
+}
+</style>
--- packages/docs/index.md
+++ packages/docs/index.md
@@ -6,7 +6,7 @@ titleTemplate: Composition-first Vue state management
 hero:
   name: Bublina
   text: Composition-first Vue state management 
-  tagline: A simple and straight-forward global state management built for composition. 
+  tagline: A simple and straightforward global state management built for composition. 
   actions:
     - theme: brand
       text: Get Started
@@ -21,7 +21,7 @@ hero:
 features:
 - title: Simple and easy to use
   details: If you've ever touched a composition API, you'll feel right at home.
-- title: '"Not so global" state management'
+- title: '"Not-so-global" state management'
   details: A robust instancing system that allows running multiple stores of the same type out of the box.
 - title: No boilerplate
   details: No need for a complicated actions, mutations and getters setup. It's like writing a composable!
