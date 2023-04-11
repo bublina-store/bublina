55a73b7c69fe728cf9c0f0ab705087198e088cfa
TomasDurica <tomas.durica.sk@gmail.com>
2023-04-11 22:21:50 +0200

    Fixed store types not correctly inferred in strict mode

--- packages/store/types.ts
+++ packages/store/types.ts
@@ -3,4 +3,4 @@ import { Ref } from 'vue'
 export type Fn<TArgs extends readonly unknown[] = unknown[], TReturn = unknown> = (...args: TArgs) => TReturn
 
 export type StoreName = string
-export type Store = Ref<unknown> | Record<string, Ref<unknown> | Fn>
+export type Store = Ref<unknown> | Record<string, Ref<unknown> | Fn<any[], any>>
